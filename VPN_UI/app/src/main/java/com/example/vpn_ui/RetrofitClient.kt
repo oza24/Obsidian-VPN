538fabc9a35b31bc100410117ea6ca2a7dcec46a
Vilas Oza <vilasoza19@gmail.com>
2026-04-16 13:36:36 +0530

    fixed security issues and updated frontend

--- VPN_UI/app/src/main/java/com/example/vpn_ui/RetrofitClient.kt
+++ VPN_UI/app/src/main/java/com/example/vpn_ui/RetrofitClient.kt
@@ -1,27 +1,4 @@
-//package com.example.vpn_ui
-//
-//import android.content.Context
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//
-//object RetrofitClient {
-//
-//    private const val BASE_URL = "http://192.168.56.1:5000/"  // keep your existing IP
-//
-//    val api: ApiService by lazy {
-//        Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//            .create(ApiService::class.java)
-//    }
-//
-//    // Helper - call this everywhere instead of writing "Bearer $token" manually
-//    suspend fun getAuthHeader(context: Context): String {
-//        val token = TokenManager.getToken(context) ?: ""
-//        return "Bearer $token"
-//    }
-//}
+
 
 
 
@@ -34,7 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     // ← Keep your existing IP here
-    private const val BASE_URL = "http://192.168.237.115:5000/"
+    private const val BASE_URL = "http://192.168.237.115:5000/" // keep your existing IP 
 
     val api: ApiService by lazy {
         Retrofit.Builder()
